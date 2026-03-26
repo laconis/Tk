594ad727fada20d221dbdcb19319ee4422f35cf1
laconis <laconis.dev@gmail.com>
2026-03-26 12:01:11 +0100

    Update header.php

--- header.php
+++ header.php
@@ -3,6 +3,15 @@
 <head>
     <title>Console d'administration</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+
+<style>
+    body { background: #f8f9fa; }
+    .menu a { margin-right: 15px; }
+    .card { margin-bottom: 20px; }
+</style>
 </head>
 <body>
 
