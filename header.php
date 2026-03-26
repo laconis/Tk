d4b339ad13aa9b643bdecdbfb761673f5e424971
laconis <laconis.dev@gmail.com>
2026-03-26 11:44:44 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Console d'administration</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<nav class="menu">
+    <a href="index.php">🏠 Dashboard</a>
+    <a href="logs.php">📄 Logs</a>
+    <a href="status.php">🟢 Statut</a>
+    <a href="config.php">⚙️ Config</a>
+</nav>
+
+<div class="content">
