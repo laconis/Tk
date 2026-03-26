e150ad350e8e3ffb70ee5cb18cbe9de5e8b7aae4
laconis <laconis.dev@gmail.com>
2026-03-26 11:55:45 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php include "header.php"; ?>
+
+<h1>Configuration</h1>
+
+<p>Cette page pourra contenir :</p>
+
+<ul>
+    <li>Chemins source / destination</li>
+    <li>Paramètres MySQL</li>
+    <li>Durée du heartbeat</li>
+    <li>Activation/désactivation des logs</li>
+    <li>Paramètres du script Python</li>
+</ul>
+
+<p>Elle est prête à être complétée selon tes besoins.</p>
+
+<?php include "footer.php"; ?>
